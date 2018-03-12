1e09a10ae87583cc0931cf510e002a0f7447f672
Maxcell Wilson <maxcell.wilson@gmail.com>
2018-03-12 11:23:55 -0400

    Update with intro js lecture additions

---
+++ 6_intro_to_js/index.js
@@ -0,0 +1,90 @@
+/*
+Data Types in Ruby
+- Strings
+- Arrays
+- Booleans
+- Objects
+- Classes
+- Fixnums
+- Floats
+
+Data Types in JavaScript
+- Strings
+- Objects
+- Numbers
+- Booleans
+- Arrays
+- undefined
+- null
+*/
+
+// Objects - Key/Values (similar but not the same as a hash in Ruby)
+// let beef = {
+//   steak: "cow",
+//   vegan: {
+//     vegetables: "broccoli"
+//   }
+// }
+
+// Function Declaraction
+// function foo(){
+//   console.log("Hello World")
+// }
+
+// Function Expression
+// let bar = function(){
+//   console.log("Hello World")
+// }
+
+// Functions with inputs
+// function foo(cool_input){
+//   console.log(cool_input)
+// }
+//
+// foo("Hello World")
+// let a = "Billy Joe"
+// let b = "Laura"
+
+// let arr = [1,2,3,4,5,6]
+// let obj = {
+//   foo: "bar",
+//   beef: "steak",
+//   name: "Prince"
+// }
+
+// For loop with basic syntax
+// for(let i = 0; i < arr.length; i++){
+//   console.log(arr[i])
+// }
+
+// For loop with for...of (arrays)
+// for(let element of arr){
+//   console.log(element)
+// }
+
+// For loop with for...in (objects)
+// for(let element in obj){
+//   console.log(`${element}: ${obj[element]}`)
+// }
+
+// Control Flow with If
+// if( a === b ){
+//   console.log("Wow these are cool people")
+// } else if (a !== b) {
+//   console.log(`I really like these two people: ${a} & ${b}`)
+// } else {
+//   console.log("Wow how are they not the same")
+// }
+
+// Control Flow with Switch
+// switch (a) {
+//   case 'Prince':
+//     console.log("This is the reaction to Prince!")
+//   break;
+//   case 'Laura':
+//     console.log("This is the reaction to Laura")
+//   break;
+//   default:
+//     console.log("WOW WHO ARE YOU?!")
+//   break;
+// }
